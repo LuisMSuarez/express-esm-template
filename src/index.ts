9e97babe6a17e08a2b8e5c4b972b0d50b87834ae
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-12-29 17:25:35 -0800

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/ping', (_req, res) => {
-  res.json({ message: 'pong' });
+app.get("/ping", (_req, res) => {
+  res.json({ message: "pong" });
 });
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server listening on http://localhost:${port}`)
+);
 
 export default app;
