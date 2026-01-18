686072cae40921cef181a101362ea701927754f5
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2026-01-17 19:39:33 -0800

    Introduce IPingService interface and refactor usage
    Added a new IPingService interface and updated PingService, PingController, and related tests to use the interface for improved abstraction and testability. Updated dependency injection bindings to reference the interface.

--- src/container.ts
+++ src/container.ts
@@ -2,12 +2,16 @@ import { Container } from "inversify";
 import { TYPES } from "./types.js";
 import { PingService } from "./services/ping.service.js";
 import { PingController } from "./controllers/ping.controller.js";
+import { IPingService } from "./services/ping.service.interface.js";
 
 // Setup of IOC container
 const container = new Container();
 
 // Registration of classes
-container.bind(TYPES.PingService).to(PingService).inRequestScope();
+container
+  .bind<IPingService>(TYPES.PingService)
+  .to(PingService)
+  .inRequestScope();
 container.bind(TYPES.PingController).to(PingController).inRequestScope();
 
 export { container };
